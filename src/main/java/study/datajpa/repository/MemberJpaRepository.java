17f7af02ede73d975e44a35084c9fc66249f33cf
ulzcq <syj9913@ynu.ac.kr>
2022-09-17 01:42:24 +0900

    순수 JPA 벌크성 수정 쿼리 테스트

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -62,4 +62,14 @@ public class MemberJpaRepository {
                 .setParameter("age", age)
                 .getSingleResult();
     }
+
+    /** JPA를 사용한 벌크성 수정 쿼리 */
+    public int bulkAgePlus(int age) {
+        int resultCount = em.createQuery(
+                "update Member m set m.age = m.age + 1" +
+                        "where m.age >= :age")
+                .setParameter("age", age)
+                .executeUpdate();
+        return resultCount;
+    }
 }
