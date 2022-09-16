43d59fdb732f9584332bb2bdb40f6512d5a370ca
ulzcq <syj9913@ynu.ac.kr>
2022-09-16 13:48:18 +0900

    쿼리 메소드 - 메소드 이름으로 쿼리 생성
    메소드 이름을 분석해서 JPQL 쿼리 실행(스프링 데이터 JPA 공식 문서 참고)
    이름과 나이를 기준으로 회원을 조회
    
    Issues: #2

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 /**
  * 공통 인터페이스
  * - JavaConfig 설정: 스프링 부트 사용 시 생략 가능(@SpringBootApplication 위치를 지정)
@@ -13,4 +15,5 @@ import study.datajpa.entity.Member;
  * - 제네릭 <엔티티타입, 식별자타입> 설정
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
