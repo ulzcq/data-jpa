6c59a8b9e15f6798c57997909df2eddccadaffd6
ulzcq <syj9913@ynu.ac.kr>
2022-09-18 18:28:37 +0900

    Web 확장 - 도메인 클래스 컨버터
    HTTP 파라미터로 넘어온 엔티티의 아이디로 엔티티 객체를 찾아서 바인딩
    사실 외부에 PK를 공개하거나 쿼리가 단순한 경우가 생각보다 많지가 않다.
    간단간단할 경우에만 사용, 복잡해지면 못쓴다고 함(권장X..)

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,42 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    /** 도메인 클래스 컨버터 사용 전 */
+    @GetMapping("/members/{id}")
+    public String findMember1(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    /**
+     * 도메인 클래스 컨버터 사용 후
+     * - HTTP 요청은 회원 id 를 받지만 도메인 클래스 컨버터가 중간에 동작해서 회원 엔티티 객체를 반환
+     * - 도메인 클래스 컨버터도 리파지토리를 사용해서 엔티티를 찾음
+     *
+     * ※ 주의: 도메인 클래스 컨버터로 엔티티를 파라미터로 받으면, 이 엔티티는 단순 조회용으로만 사용해야 한다.
+     * (트랜잭션이 없는 범위에서 엔티티를 조회했으므로, 엔티티를 변경해도 DB에 반영되지 않는다.)
+     */
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init(){
+        memberRepository.save(new Member("userA"));
+    }
+}
